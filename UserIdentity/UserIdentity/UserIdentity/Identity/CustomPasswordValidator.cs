04d2ba9c301a369c21908aa5b8c121d1eefd0653
Huseyn <>
2018-09-27 02:54:57 +0400

    Update

---
+++ UserIdentity/UserIdentity/UserIdentity/Identity/CustomPasswordValidator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace UserIdentity.Identity
+{
+    public class CustomPasswordValidator:PasswordValidator
+    {
+        public override async Task<IdentityResult> ValidateAsync(string password)
+        {
+            var result = await base.ValidateAsync(password);
+
+            if (password.Contains("12345"))
+            {
+                var errors = result.Errors.ToList();
+                errors.Add("Password cannot contain consecutive numeric expression");
+                result = new IdentityResult(errors);
+            }
+            return result;
+        }
+    }
+}
